6bd0bb327afce457d2b4d669455a72265b9267c8
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-24 15:45:34 +0000

    interface completed

--- js/interface.js
+++ js/interface.js
@@ -1,15 +1,42 @@
+
 var widgetId = Fliplet.Widget.getDefaultId();
 var data = Fliplet.Widget.getData(widgetId) || {};
-var headingValue = data.heading || "Welcome to the login of this app";
-$('#login_heading').val(headingValue).trigger('change');
+var organizationId = Fliplet.Env.get('organizationId');
+var validInputEventName = 'interface-validate';
 
-var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
+var allDataSources;
+var templates = {
+  dataSourceEntry: template('data-source-entry')
+};
+
+var fields = [
+  'dataSource',
+  'emailColumn',
+  'passColumn'
+];
+
+var loginActionProvider = Fliplet.Widget.open('com.fliplet.link', {
   // If provided, the iframe will be appended here,
   // otherwise will be displayed as a full-size iframe overlay
-  selector: '#action',
+  selector: '#login-link-action',
   // Also send the data I have locally, so that
   // the interface gets repopulated with the same stuff
-  data: data.action,
+  data: data.loginAction,
+  // Events fired from the provider
+  onEvent: function (event, data) {
+    if (event === 'interface-validate') {
+      Fliplet.Widget.toggleSaveButton(data.isValid === true);
+    }
+  }
+});
+
+var resetActionProvider = Fliplet.Widget.open('com.fliplet.link', {
+  // If provided, the iframe will be appended here,
+  // otherwise will be displayed as a full-size iframe overlay
+  selector: '#reset-link-action',
+  // Also send the data I have locally, so that
+  // the interface gets repopulated with the same stuff
+  data: data.resetAction,
   // Events fired from the provider
   onEvent: function (event, data) {
     if (event === 'interface-validate') {
@@ -26,17 +53,32 @@ Fliplet.Widget.onSaveRequest(function () {
 // 2. Fired when the user submits the form
 $('form').submit(function (event) {
   event.preventDefault();
-  linkActionProvider.forwardSaveRequest();
+  loginActionProvider.forwardSaveRequest();
+  resetActionProvider.forwardSaveRequest();
 });
 
 // 3. Fired when the provider has finished
-linkActionProvider.then(function (result) {
-  data.action = result.data;
+loginActionProvider.then(function (result) {
+  data.loginAction = result.data;
   save(true);
 });
 
+resetActionProvider.then(function (result) {
+  data.resetAction = result.data;
+  save(true);
+});
+
+// Function to compile a Handlebars template
+function template(name) {
+  return Handlebars.compile($('#template-' + name).html());
+}
+
 function save(notifyComplete) {
-  data.heading = $('#login_heading').val();
+  // Get and save values to data
+  fields.forEach(function (fieldId) {
+    data[fieldId] = $('#' + fieldId).val();
+  });
+
   Fliplet.Widget.save(data).then(function () {
     if (notifyComplete) {
       Fliplet.Widget.complete();
@@ -47,41 +89,85 @@ function save(notifyComplete) {
   });
 }
 
-$('#select_datasource').on('change', function() {
-  var selectedValue = $(this).val();
-  var selectedText = $(this).find("option:selected").text();
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+Fliplet.Widget.emit(validInputEventName, {
+  isValid: false
+});
 
-  if (selectedValue !== "") {
+Fliplet.DataSources.get({ organizationId: organizationId }).then(function (dataSources) {
+  allDataSources = dataSources;
+  dataSources.forEach(renderDataSource);
+  return Promise.resolve();
+}).then(initialiseData);
+
+function renderDataSource(dataSource){
+  $('#dataSource').append(templates.dataSourceEntry(dataSource));
+}
+
+function renderDataSourceColumn(dataSourceColumn){
+  $('#emailColumn').append('<option value="'+dataSourceColumn+'">'+dataSourceColumn+'</option>');
+  $('#passColumn').append('<option value="'+dataSourceColumn+'">'+dataSourceColumn+'</option>');
+}
+
+$('#dataSource').on('change', function onDataSourceListChange() {
+  var selectedOption = $(this).find("option:selected"),
+      selectedText = selectedOption.text(),
+      selectedValue = selectedOption.val();
+
+  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+  $('#emailColumn option:gt(0)').remove();
+  $('#passColumn option:gt(0)').remove();
+
+  if ( $(this).val() !== "none" ) {
     $('#select-email-field').removeClass('hidden');
     $('#select-pass-field').removeClass('hidden');
   } else {
     $('#select-email-field').addClass('hidden');
     $('#select-pass-field').addClass('hidden');
   }
+
+  allDataSources.forEach(function(dataSource){
+    if(dataSource.id == selectedValue && typeof dataSource.columns !== "undefined") {
+      dataSource.columns.forEach(renderDataSourceColumn);
+    }
+  });
 });
 
-$('#select_email, #select_pass').on('change', function() {
+$('#emailColumn, #passColumn').on('change', function() {
   var selectedValue = $(this).val();
   var selectedText = $(this).find("option:selected").text();
   $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+
+  Fliplet.Widget.emit(validInputEventName, {
+    isValid: selectedValue !== 'none'
+  });
 });
 
-$('#allow_reset').on('change', function() {
+$('#allow_reset').on('change', $.debounce(function() {
+
   if ( $(this).is(':checked') ) {
-    $('.reset-pass-redirect').removeClass('hidden');
+  	$('.reset-pass-redirect').removeClass('hidden');
+    data.allowReset = true;
   } else {
-    $('.reset-pass-redirect').addClass('hidden');
+  	$('.reset-pass-redirect').addClass('hidden');
+    data.allowReset = false;
   }
-});
 
-
-
-
-$('#login_heading').on('keyup change paste blur', $.debounce(function() {
   save();
-}, 500));
+
+}, 0));
 
 $('#help_tip').on('click', function() {
   alert("During beta, please use live chat and let us know what you need help with.");
 });
+
+function initialiseData() {
+  fields.forEach(function (fieldId) {
+    if(data[fieldId]) {
+      $('#' + fieldId).val(data[fieldId]).change();
+    }
+  });
+
+  if ( data.allowReset ) {
+    $('#allow_reset').trigger('change');
+  }
+}
