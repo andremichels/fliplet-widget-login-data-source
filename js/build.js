3a9f578c70e3feedbcf59f8b8c32c1a9e90a1843
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-24 18:07:50 +0000

    Tweaks

--- js/build.js
+++ js/build.js
@@ -1,8 +1,8 @@
 $('[data-login-ds-id]').each(function(){
   var _this = this;
-  this.$container = $(this);
-  this.id = this.$container.attr('data-login-ds-id');
-  data = Fliplet.Widget.getData(this.id);
+  var $container = $(this);
+  var id = $container.attr('data-login-ds-id');
+  var data = Fliplet.Widget.getData(id);
 
   this.pvName = 'login_data_source_component_' + this.id;
   var dataStructure = {
@@ -148,14 +148,14 @@ $('[data-login-ds-id]').each(function(){
 
   function attachEventListeners() {
 
-    this.$container.find('.btn-login').on('click', function() {
+    $container.find('.btn-login').on('click', function() {
       var _this = $(this);
       _this.parents('.form-btns').find('.text-danger').addClass('hidden');
 
-      window.profileEmail = this.$container.find('input.profile_email').val().toLowerCase(); // GET EMAIL VALUE
+      window.profileEmail = $container.find('input.profile_email').val().toLowerCase(); // GET EMAIL VALUE
 
       // @TODO: SALT password entered by user and save it in the var below for comparisson with the SALTED password already stored in DS
-      window.profilePassword = this.$container.find('input.profile_password').val();
+      window.profilePassword = $container.find('input.profile_password').val();
 
       // Triggers loading
       $(this).addClass('loading');
@@ -207,39 +207,39 @@ $('[data-login-ds-id]').each(function(){
     // EVENT LISTENER FOR FORGET PASSWORD RESET
     // Just switches views Login to Email verification
     // Leave as it is
-    this.$container.find('.btn-forget-pass').on('click', function() {
-    	$('.fl-login-holder').fadeOut(100);
+    $container.find('.btn-forget-pass').on('click', function() {
+    	$container.find('.fl-login-holder').fadeOut(100);
       setTimeout(function() {
-      	$('.fl-restore-pass').fadeIn(250);
-    		calculateElHeight( this.$container.find('.state[data-state=verify-email]') );
+      	$container.find('.fl-restore-pass').fadeIn(250);
+    		calculateElHeight( $container.find('.state[data-state=verify-email]') );
       }, 100);
     });
 
 
-    this.$container.find('.back-login').on('click', function() {
-    	$('.fl-restore-pass').fadeOut(100);
+    $container.find('.back-login').on('click', function() {
+    	$container.find('.fl-restore-pass').fadeOut(100);
       setTimeout(function() {
-      	$('.fl-login-holder').fadeIn(250);
+      	$container.find('.fl-login-holder').fadeIn(250);
 
         // Reset states of email verification
-        this.$container.find('.reset-email-error').addClass('hidden');
-        this.$container.find('.pin-verify-error').addClass('hidden');
-        this.$container.find('.pin-sent-error').addClass('hidden');
-    		$('.state').removeClass('present past').addClass('future');
-        $('.state[data-state=verify-email]').removeClass('future').addClass('start');
+        $container.find('.reset-email-error').addClass('hidden');
+        $container.find('.pin-verify-error').addClass('hidden');
+        $container.find('.pin-sent-error').addClass('hidden');
+    		$container.find('.state').removeClass('present past').addClass('future');
+        $container.find('.state[data-state=verify-email]').removeClass('future').addClass('start');
       }, 100);
     });
 
-    this.$container.find('.verify-identity').on('click', function(event) {
+    $container.find('.verify-identity').on('click', function(event) {
       var _this = $(this);
       _this.addClass("disabled");
 
-      window.resetEmail = this.$container.find('input.reset-email-field').val().toLowerCase(); // Get email for reset
+      window.resetEmail = $container.find('input.reset-email-field').val().toLowerCase(); // Get email for reset
 
-      this.$container.find('.reset-email-error').removeClass('show');
+      $container.find('.reset-email-error').removeClass('show');
       // EMAIL FOUND ON DATA SOURCE
-      if (this.$container.find('.state[data-state=verify-email] .form-group').hasClass('has-error')) {
-        this.$container.find('.state[data-state=verify-email] .form-group').removeClass('has-error');
+      if ($container.find('.state[data-state=verify-email] .form-group').hasClass('has-error')) {
+        $container.find('.state[data-state=verify-email] .form-group').removeClass('has-error');
       }
 
       // VALIDATE EMAIL
@@ -249,36 +249,36 @@ $('[data-login-ds-id]').each(function(){
           // EMAIL FOUND ON DATA SOURCE
           userDataPV.email = emailAddress;
 
-          if (this.$container.find('.state[data-state=verify-email] .form-group').hasClass('has-error')) {
-            this.$container.find('.state[data-state=verify-email] .form-group').removeClass('has-error');
+          if ($container.find('.state[data-state=verify-email] .form-group').hasClass('has-error')) {
+            $container.find('.state[data-state=verify-email] .form-group').removeClass('has-error');
           }
           sendNotification(resetEmail, function () {
             // TRANSITION
-            this.$container.find('.state.present').removeClass('present').addClass('past');
+            $container.find('.state.present').removeClass('present').addClass('past');
 
-            this.$container.find('.verify-user-email').text(resetEmail); // UPDATES TEXT WITH EMAIL
+            $container.find('.verify-user-email').text(resetEmail); // UPDATES TEXT WITH EMAIL
             _this.removeClass("disabled");
 
-            calculateElHeight(this.$container.find('.state[data-state=verify-code]'));
-            this.$container.find('.state[data-state=verify-code]').removeClass('future').addClass('present');
+            calculateElHeight($container.find('.state[data-state=verify-code]'));
+            $container.find('.state[data-state=verify-code]').removeClass('future').addClass('present');
           }, function () {
-            this.$container.find('.reset-email-error').text(CONTACT_UNREACHABLE).removeClass('hidden');
+            $container.find('.reset-email-error').text(CONTACT_UNREACHABLE).removeClass('hidden');
           });
 
         }, function ( error ) {
           if ( error ) {
             // EMAIL NOT FOUND ON DATA SOURCE
             _this.removeClass("disabled");
-            this.$container.find('.reset-email-error').html("We couldn't find your email in our system. Please try again.");
-            this.$container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
-            calculateElHeight(this.$container.find('.state[data-state=verify-email]'));
+            $container.find('.reset-email-error').html("We couldn't find your email in our system. Please try again.");
+            $container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
+            calculateElHeight($container.find('.state[data-state=verify-email]'));
           } else {
             // EMAIL FOUND ON DATA SOURCE BUT IT'S NOT REGISTERED
             // MEANS NO PASSWORD FOUND
             _this.removeClass("disabled");
-            this.$container.find('.reset-email-error').html("You don't seem to be registered in our system. Please try registering first.");
-            this.$container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
-            calculateElHeight(this.$container.find('.state[data-state=verify-email]'));
+            $container.find('.reset-email-error').html("You don't seem to be registered in our system. Please try registering first.");
+            $container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
+            calculateElHeight($container.find('.state[data-state=verify-email]'));
           }
 
         });
@@ -286,71 +286,71 @@ $('[data-login-ds-id]').each(function(){
       } else {
         // INVALID EMAIL
         _this.removeClass("disabled");
-        this.$container.find('.reset-email-error').html("Please enter a valid email address and try again.");
-        this.$container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
-        calculateElHeight(this.$container.find('.state[data-state=verify-email]'));
+        $container.find('.reset-email-error').html("Please enter a valid email address and try again.");
+        $container.find('.state[data-state=verify-email] .form-group').addClass('has-error');
+        calculateElHeight($container.find('.state[data-state=verify-email]'));
       }
     });
 
-    this.$container.find('.back.start').on('click', function() {
-      this.$container.find('.state.present').removeClass('present').addClass('future');
+    $container.find('.back.start').on('click', function() {
+      $container.find('.state.present').removeClass('present').addClass('future');
 
-      this.$container.find('.reset-email-field').val(""); // RESETS EMAIL VALUE
-      this.$container.find('.pin-code-field').val(""); // RESETS PIN
+      $container.find('.reset-email-field').val(""); // RESETS EMAIL VALUE
+      $container.find('.pin-code-field').val(""); // RESETS PIN
 
       // REMOVES ERROR MESSAGE ON CURRENT STATE IF THERE IS ONE
-      if (this.$container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
-        this.$container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
+      if ($container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
+        $container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
       }
 
       //check the validation current state.
       if (userDataPV.code !== "" && userDataPV.code_generated_at > Date.now() - (CODE_VALID * 60 * 1000)) {
-        this.$container.find('.have-code').removeClass('hidden');
+        $container.find('.have-code').removeClass('hidden');
       }
-      //this.$container.find('.verify-code').html("Verify").removeClass("disabled");
-      this.$container.find('.authenticate').removeClass('loading');
-      this.$container.find('.authenticate').find('span').removeClass('hidden');
-      this.$container.find('.authenticate').find('.loader').removeClass('show');
+      //$container.find('.verify-code').html("Verify").removeClass("disabled");
+      $container.find('.authenticate').removeClass('loading');
+      $container.find('.authenticate').find('span').removeClass('hidden');
+      $container.find('.authenticate').find('.loader').removeClass('show');
 
-      calculateElHeight(this.$container.find('.state[data-state=verify-email]'));
-      this.$container.find('.state[data-state=verify-email]').removeClass('past').addClass('present');
+      calculateElHeight($container.find('.state[data-state=verify-email]'));
+      $container.find('.state[data-state=verify-email]').removeClass('past').addClass('present');
     });
 
-    this.$container.find('.have-code').on('click', function() {
+    $container.find('.have-code').on('click', function() {
       // TRANSITION
-      this.$container.find('.state.present').removeClass('present').addClass('past');
-      this.$container.find('.verify-user-email').text(userDataPV.email); // UPDATES TEXT WITH EMAIL
+      $container.find('.state.present').removeClass('present').addClass('past');
+      $container.find('.verify-user-email').text(userDataPV.email); // UPDATES TEXT WITH EMAIL
 
-      calculateElHeight(this.$container.find('.state[data-state=verify-code]'));
-      this.$container.find('.state[data-state=verify-code]').removeClass('future').addClass('present');
+      calculateElHeight($container.find('.state[data-state=verify-code]'));
+      $container.find('.state[data-state=verify-code]').removeClass('future').addClass('present');
     });
 
-    this.$container.find('.authenticate').on('click', function() {
+    $container.find('.authenticate').on('click', function() {
     	var _this = $(this);
 
-      this.$container.find('.pin-verify-error').addClass('hidden');
-      this.$container.find('.pin-sent-error').addClass('hidden');
+      $container.find('.pin-verify-error').addClass('hidden');
+      $container.find('.pin-sent-error').addClass('hidden');
       // Simulates loading
       $(this).addClass('loading');
       $(this).find('span').addClass('hidden');
       $(this).find('.loader').addClass('show');
 
-      var userPin = this.$container.find('.pin-code-field').val(),
+      var userPin = $container.find('.pin-code-field').val(),
           codeIsValid = userDataPV.code_generated_at > Date.now() - (CODE_VALID * 60 * 1000);
 
       // VERIFY PIN CODE
       if (userPin === userDataPV.code) {
         if (!codeIsValid) {
-          this.$container.find('.state[data-state=verify-code] .form-group').addClass('has-error');
-          this.$container.find('.resend-code').removeClass('hidden');
+          $container.find('.state[data-state=verify-code] .form-group').addClass('has-error');
+          $container.find('.resend-code').removeClass('hidden');
           _this.removeClass('loading');
           _this.find('span').removeClass('hidden');
           _this.find('.loader').removeClass('show');
-          this.$container.find('.pin-verify-error').removeClass('hidden');
-          calculateElHeight(this.$container.find('.state[data-state=verify-code]'));
+          $container.find('.pin-verify-error').removeClass('hidden');
+          calculateElHeight($container.find('.state[data-state=verify-code]'));
         } else {
-          if (this.$container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
-            this.$container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
+          if ($container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
+            $container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
           }
 
           userDataPV.verified = true;
@@ -361,9 +361,9 @@ $('[data-login-ds-id]').each(function(){
             _this.find('span').removeClass('hidden');
             _this.find('.loader').removeClass('show');
 
-            this.$container.find('.state.present').removeClass('present').addClass('past');
-            calculateElHeight(this.$container.find('.state[data-state=all-done]'));
-            this.$container.find('.state[data-state=all-done]').removeClass('future').addClass('present');
+            $container.find('.state.present').removeClass('present').addClass('past');
+            calculateElHeight($container.find('.state[data-state=all-done]'));
+            $container.find('.state[data-state=all-done]').removeClass('future').addClass('present');
           });
         }
       } else {
@@ -371,40 +371,40 @@ $('[data-login-ds-id]').each(function(){
         _this.find('span').removeClass('hidden');
         _this.find('.loader').removeClass('show');
 
-        this.$container.find('.state[data-state=verify-code] .form-group').addClass('has-error');
-        this.$container.find('.pin-verify-error').removeClass('hidden');
-        calculateElHeight(this.$container.find('.state[data-state=verify-code]'));
+        $container.find('.state[data-state=verify-code] .form-group').addClass('has-error');
+        $container.find('.pin-verify-error').removeClass('hidden');
+        calculateElHeight($container.find('.state[data-state=verify-code]'));
       }
     });
 
-    this.$container.find('.resend-code').on('click', function () {
-      this.$container.find('.pin-sent-error').addClass('hidden');
-      this.$container.find('.pin-sent-success').addClass('hidden');
-      this.$container.find('.pin-code-field').val("");
-      if (this.$container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
-        this.$container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
+    $container.find('.resend-code').on('click', function () {
+      $container.find('.pin-sent-error').addClass('hidden');
+      $container.find('.pin-sent-success').addClass('hidden');
+      $container.find('.pin-code-field').val("");
+      if ($container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
+        $container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
       }
-      if (!this.$container.find('.resend-code').hasClass('hidden')) {
-        this.$container.find('.resend-code').addClass('hidden');
+      if (!$container.find('.resend-code').hasClass('hidden')) {
+        $container.find('.resend-code').addClass('hidden');
       }
 
-      calculateElHeight(this.$container.find('.state[data-state=verify-code]'));
+      calculateElHeight($container.find('.state[data-state=verify-code]'));
 
       sendNotification(emailAddress, function () {
-        this.$container.find('.pin-code-field').val("");
-        this.$container.find('.pin-sent-success').removeClass('hidden');
-        if (this.$container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
-          this.$container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
+        $container.find('.pin-code-field').val("");
+        $container.find('.pin-sent-success').removeClass('hidden');
+        if ($container.find('.state[data-state=verify-code] .form-group').hasClass('has-error')) {
+          $container.find('.state[data-state=verify-code] .form-group').removeClass('has-error');
         }
-        if (!this.$container.find('.resend-code').hasClass('hidden')) {
-          this.$container.find('.resend-code').addClass('hidden');
+        if (!$container.find('.resend-code').hasClass('hidden')) {
+          $container.find('.resend-code').addClass('hidden');
         }
       }, function () {
-        this.$container.find('.pin-sent-error').text(CONTACT_UNREACHABLE).removeClass("hidden");
+        $container.find('.pin-sent-error').text(CONTACT_UNREACHABLE).removeClass("hidden");
       });
     });
 
-    this.$container.find('.reset-continue').on('click', function () {
+    $container.find('.reset-continue').on('click', function () {
       if(typeof data.resetAction !== "undefined") {
         Fliplet.Navigate.to(data.resetAction);
       }
@@ -431,7 +431,7 @@ $('[data-login-ds-id]').each(function(){
 
     $(document).ready(initEmailValidation);
 
-    this.$container.on("fliplet_page_reloaded", initEmailValidation);
+    $container.on("fliplet_page_reloaded", initEmailValidation);
   } else {
     document.addEventListener("deviceready", initEmailValidation);
   }
